b2911f7846a8aed7bd3c1d086922e8e3f6cfdbad
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2021-10-09 21:38:46 -0300

    Exemplos JavaScript

---
+++ JavaScript/media.js
@@ -0,0 +1,15 @@
+let nota1 = 5
+let nota2 = 8.5
+let nota3 = 4
+let nota4 = 5
+let media
+
+media = (nota1 + nota2 + nota3 + nota4)/4
+
+console.log(media)
+
+if (media > 7){
+    console.log('Você foi aprovado')
+} else {
+    console.log('Você foi reprovado')
+}
