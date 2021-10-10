43447ec931bd72f3b70f42fab0636aa8ea315f73
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2021-10-09 21:43:05 -0300

    JS Exemplos

---
+++ JavaScript/velocidade.js
@@ -0,0 +1,9 @@
+let velocidade = 50.5
+
+console.log(`Sua velocidade é ${velocidade}`)
+
+if (velocidade > 60){
+    console.log('Você está acima da velocidade mínima! Você está sendo multado!')
+}
+
+console.log('Sempre use cinto de segurança!')
