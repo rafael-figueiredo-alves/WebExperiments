1092dbf8b615b3d376c576cb873c2ea9f88cc030
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2021-10-10 21:16:42 -0300

    Exercício 006

--- ExercíciosJS/Exercício 001/script.js
+++ ExercíciosJS/Exercício 001/script.js
@@ -3,15 +3,18 @@ function Carregar() {
     let hora = data.getHours();
     let msg = window.document.querySelector('div.msg');
     let foto = window.document.querySelector('img.foto');
-
-    msg.innerHTML = `<p>Agora são ${hora} horas.</p>`;
     
-
     if (hora >= 0 && hora < 12){
-        foto.src = './images/fotomanha.png';
+        msg.innerHTML = `<p><strong>Bom dia!</strong> Agora são ${hora} horas.</p>`;
+        foto.src = './images/fotodia.png';
+        window.document.body.style.backgroundColor = '#f2d2b5';
     } else if (hora >= 12 && hora <18){
+        msg.innerHTML = `<p><strong>Boa tarde!</strong> Agora são ${hora} horas.</p>`;
         foto.src = './images/fototarde.png';
+        window.document.body.style.backgroundColor = '#f35044';
     } else {
+        msg.innerHTML = `<p><strong>Boa noite!</strong> Agora são ${hora} horas.</p>`;
         foto.src = './images/fotonoite.png';
+        window.document.body.style.backgroundColor = '#4e788a';
     }
 }
