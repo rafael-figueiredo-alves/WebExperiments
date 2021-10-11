da4b338886b7ff472cd6a860e9ce1a6d240d90a1
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2021-10-10 21:25:28 -0300

    exercícios

--- ExercíciosJS/Exercício 001/script.js
+++ ExercíciosJS/Exercício 001/script.js
@@ -5,14 +5,17 @@ function Carregar() {
     let foto = window.document.querySelector('img.foto');
     
     if (hora >= 0 && hora < 12){
+        window.document.title = 'Bom dia!';
         msg.innerHTML = `<p><strong>Bom dia!</strong> Agora são ${hora} horas.</p>`;
         foto.src = './images/fotodia.png';
         window.document.body.style.backgroundColor = '#f2d2b5';
     } else if (hora >= 12 && hora <18){
+        window.document.title = 'Boa tarde!'
         msg.innerHTML = `<p><strong>Boa tarde!</strong> Agora são ${hora} horas.</p>`;
         foto.src = './images/fototarde.png';
         window.document.body.style.backgroundColor = '#f35044';
     } else {
+        window.document.title = 'Boa noite!'
         msg.innerHTML = `<p><strong>Boa noite!</strong> Agora são ${hora} horas.</p>`;
         foto.src = './images/fotonoite.png';
         window.document.body.style.backgroundColor = '#4e788a';
