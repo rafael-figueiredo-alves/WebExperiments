9f2c05d91f1642ab30ec7f52597221c5d43774f4
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2021-10-24 21:20:37 -0300

    loop JS

---
+++ JavaScript/loop_001.js
@@ -0,0 +1,15 @@
+/*
+let contador = 1
+
+while (contador <= 10) {
+    console.log(contador)
+    contador ++
+}
+*/
+
+let contador = 1
+
+do {
+    console.log(contador)
+    contador ++
+} while (contador <= 10)
