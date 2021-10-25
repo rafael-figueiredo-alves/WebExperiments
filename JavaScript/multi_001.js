cb2f9629b17ec48f70a5a0a0410881f60a68d71d
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2021-10-24 21:26:30 -0300

    multiplicação 2

--- JavaScript/multi_001.js
+++ JavaScript/multi_001.js
@@ -3,6 +3,6 @@ let resultado = 0
 
 while (contador <= 10) {
     resultado = contador * 2
-    console.log('2 X ' + contador +' = ' + resultado)
+    console.log(`2 X ${contador} = ${resultado}`)
     contador ++
 }
