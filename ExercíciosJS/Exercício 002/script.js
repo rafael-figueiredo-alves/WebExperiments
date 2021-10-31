80bec85359468b62a99aae9cc381dfaee1e56ffb
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2021-10-31 19:49:29 -0300

    Tabuada

--- ExercíciosJS/Exercício 002/script.js
+++ ExercíciosJS/Exercício 002/script.js
@@ -1,3 +1,17 @@
 function exibeTabuada() {
-    let
+    let tabuada = document.querySelector('#teste').value;
+    let contador = 0
+    let Resultado = document.querySelector('div.Resultado')
+
+    Resultado.innerHTML = ''
+
+    alert(tabuada.length)
+        
+    if (tabuada.length > 0){
+        tabuada = Number(tabuada)
+        for (contador = 0; contador <= 10; contador++){
+            let res = contador * tabuada
+            Resultado.innerHTML += `<p>${tabuada} X ${contador} = <strong>${res}</strong></p>`           
+        }
+    }
 }
