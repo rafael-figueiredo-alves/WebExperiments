b1f0702b21ba0c4754d6f85d41bef7b320feb151
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2021-10-31 19:54:56 -0300

    Script

--- ExercíciosJS/Exercício 002/script.js
+++ ExercíciosJS/Exercício 002/script.js
@@ -2,16 +2,15 @@ function exibeTabuada() {
     let tabuada = document.querySelector('#teste').value;
     let contador = 0
     let Resultado = document.querySelector('div.Resultado')
-
-    Resultado.innerHTML = ''
-
-    alert(tabuada.length)
         
     if (tabuada.length > 0){
         tabuada = Number(tabuada)
+        Resultado.innerHTML = ''
         for (contador = 0; contador <= 10; contador++){
             let res = contador * tabuada
             Resultado.innerHTML += `<p>${tabuada} X ${contador} = <strong>${res}</strong></p>`           
         }
+    } else {
+        Resultado.innerHTML = '<p><strong>Não foi possível exibir a tabuada!</strong></p><p>Informe a tabuada que deseja exibir.</p>'
     }
 }
