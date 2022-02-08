d9e25fee8a9a83e7430ae37b0e8cf92b5049173e
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2022-02-08 18:53:52 -0300

    Testes com array

---
+++ JavaScript/Arrays/array_001.js
@@ -0,0 +1,11 @@
+console.clear();
+
+let nomes = ['Rafaela', "Davi", 'Jailza', 'Rafael'];
+
+console.log(nomes);
+
+nomes.forEach(nome => {
+    console.log(nome);
+});
+
+console.log(nomes.length);
