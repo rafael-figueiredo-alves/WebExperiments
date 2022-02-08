90bb9a000992511e0f963e0b8a05777a0644780c
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2022-02-08 19:08:43 -0300

    Mais testes

--- JavaScript/Arrays/array_001.js
+++ JavaScript/Arrays/array_001.js
@@ -9,3 +9,17 @@ nomes.forEach(nome => {
 });
 
 console.log(nomes.length);
+
+(() => {
+    console.log('Teste de função')
+})();
+
+// let nomes_removidos = nomes.splice(2, 1);
+
+// console.log('Primeiro array:', nomes, 'Segundo array:', nomes_removidos);
+
+console.log(delete nomes[3]);
+
+nomes.forEach(nome => {
+    console.log(nome);
+});
